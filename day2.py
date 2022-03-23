c91c69f413427ed976f22edc32e0eca23eeb553f
Fatih-hamza <66041597+Fatih-hamza@users.noreply.github.com>
2022-03-23 21:41:03 +0100

    Update day2.py

--- day2.py
+++ day2.py
@@ -21,5 +21,18 @@ def max_subset(numbers):
         # we return the choice that will maximize the sum  
         return max(potential1, potential2)
 
-# print(plants([9, 1, 3, 9, -19, 1, 1000]))
+# print(plants([9, 1, 3, 9, -19, 1, 1000]
+
+
+# the above funtion works just fine, but there is even a better way to do this
+# we just have to notice that, by using tabulation (Dynamic programming), we will get a list that looks like this:
+# if the set is {a0, a1, a2, a3, ..., an}, then the the list made of the maximum subset sum at each iteration will be: 
+# maximumx = [max(a0 ,0), max(maximums[0], a1), max(maximums[1], a0 + a2), max(maximums[2], maximums[1] + a3) , ...]
+
+
+def optimized_max_subset(numbers):
+    potential1, potential2 = 0, 0
+    for number in numbers:
+        potential1, potential2 = potential2, max(potential2, potential1 + number)
+    return potential2
 
